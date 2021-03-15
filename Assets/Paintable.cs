4fa65306280b2c8c408376d2b0e70ec20ca02476
Emre Acikgoz <acikgozac@gmail.com>
2021-03-15 21:07:06 +0300

    PaintMesh

---
+++ Assets/Paintable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+
+    public GameObject brush;
+    public float brushSize = 0.1f;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(Ray, out hit))
+            {
+
+                var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.identity,transform);
+                go.transform.localScale = Vector3.one * brushSize;
+                
+            }
+        }
+        
+    }
+}
---
+++ Assets/RayDitection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayDitection : MonoBehaviour
+{
+
+    public int maxAmount = 100;
+    public int currentAmount;
+
+    public ProgressBar bar;
+
+
+    private void Start()
+    {
+        currentAmount = maxAmount;
+        bar.SetMaxAmount(maxAmount);
+    }
+
+
+   
+
+
+    
+
+    void Update()
+    {
+       
+
+        if (Input.GetMouseButton(0))
+        {
+
+            
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit,10000f))
+            {
+                if (hit.transform.tag == "Trigger")
+                {
+                    GetRecover(15);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+
+
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    void GetRecover(int recover)
+    {
+        currentAmount -= recover;
+        bar.SetAmount(currentAmount);
+    }
+
+}
